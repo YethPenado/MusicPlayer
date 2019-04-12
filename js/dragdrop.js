aa4aa7e71e13916ac25c8fd107cd2fa4f0167eff
YethPenado <39468032+YethPenado@users.noreply.github.com>
2019-04-11 23:54:42 -0600

    last changes

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,14 +1,14 @@
-function createDrop() {
+(function createDrop () {
   let dragged;
-
+  
   document.addEventListener('dragstart', (event) => {
     dragged = event.target;
   }, false);
-
+  
   document.addEventListener('dragover', (event) => {
     event.preventDefault();
   }, false);
-
+  
   document.addEventListener('drop', (event) => {
     if (event.target.className === 'playlist content'
         || event.target.className === 'songs content') {
@@ -16,13 +16,13 @@ function createDrop() {
       let pl = singletonSongs.playlist;
       let s = singletonSongs.songs;
       let i = dragged.getAttribute('data-index');
-
+  
       if(event.target.className === 'playlist content') {
         dragArray(pl, s, i);
       } else {
         dragArray(s, pl, i);
       }
-
+  
       let uls = event.target.parentElement.parentElement.children;
       changeIndex(uls[1].children[0]);
       changeIndex(uls[2].children[0]);
@@ -30,14 +30,13 @@ function createDrop() {
       changeContent(indexSong);
     }
   }, false);
-
+  
   audio.addEventListener('ended', () => {
     indexSong += 1;
     if(indexSong > singletonSongs.songs.length - 1) indexSong = 0;
     audio.src = singletonSongs.songs[indexSong].mp3;
     changeContent(indexSong);
     audio.play();
+    createDrop();
   });
-}
-
-createDrop();
+}());
